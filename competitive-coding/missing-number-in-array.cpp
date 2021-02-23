83309da13ecc053cb9eaa4bf168c921c759db067
rajpal-se <67093654+rajpal-se@users.noreply.github.com>
2021-02-23 22:58:54 +0530

    Update missing-number-in-array.cpp

--- competitive-coding/missing-number-in-array.cpp
+++ competitive-coding/missing-number-in-array.cpp
@@ -38,5 +38,29 @@ int MissingNumber(vector<int>& array, int n) {
 
 
 /*
+Problem: Missing number in array
 
+Given an array of size N-1 such that it can only contain distinct integers in the range of 1 to N. Find the missing element.
+
+Your Task :
+Complete the function MissingNumber() that takes array and N as input and returns the value of the missing number.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ A[i] ≤ 106
+
+Example 1:
+Input:
+N = 5
+A[] = {1,2,3,5}
+Output: 4
+
+Example 2:
+Input:
+N = 10
+A[] = {1,2,3,4,5,6,7,8,10}
+Output: 9
 */
