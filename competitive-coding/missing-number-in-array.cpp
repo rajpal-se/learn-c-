31f29d92c2ebe24b2a555def9332b09d21677b9c
rajpal-se <67093654+rajpal-se@users.noreply.github.com>
2021-02-23 23:06:30 +0530

    Update missing-number-in-array.cpp

--- competitive-coding/missing-number-in-array.cpp
+++ competitive-coding/missing-number-in-array.cpp
@@ -1,5 +1,7 @@
 /*
 Problem: Missing number in array
+Level: Easy
+
 Given an array of size N-1 such that it can only contain distinct integers in the range of 1 to N. Find the missing element.
 */
 
